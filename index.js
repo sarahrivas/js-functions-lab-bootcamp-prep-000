5f1ecef356eb50602e010f8f980be8748078a9d2
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-07 03:58:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo (holiday, name) {
 }
 
 function holidayCountdown (holiday, daysTilHoliday) {
-  return `It's ${days} days until ${holiday}!`;
+  return `It's ${daysTilHoliday} days until ${holiday}!`;
 }
